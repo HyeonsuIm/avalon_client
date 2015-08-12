4c0490cc3f8aff0325b400cb903cc2536d2950af
KimHongHoun <yumezi7@gmail.com>
2015-08-13 04:23:53 +0900

    Command 구현 귀속말 구현완

--- Avalron/Avalron/Chatting.cs
+++ Avalron/Avalron/Chatting.cs
@@ -11,7 +11,9 @@ namespace Avalron.Avalron
     {
         System.Windows.Forms.RichTextBox chattingBox = new RichTextBox();
         ComboBox chatOption = new ComboBox();
+        string[] chatOptionStr = {"전체", "귓속말" };
         TextBox chatText = new TextBox();
+        bool chatFirst = true;
 
         public Chatting(Control.ControlCollection Controls)
         {
@@ -25,19 +27,20 @@ namespace Avalron.Avalron
             
             chatOption.FormattingEnabled = true;
             chatOption.Location = new System.Drawing.Point(14, 522);
-            chatOption.Items.AddRange(new object[] {
-            "전체",
-            "귀속말"});
+            chatOption.Items.AddRange(chatOptionStr);
             chatOption.SelectedIndex = 0;
             chatOption.Name = "옵션";
             chatOption.Size = new System.Drawing.Size(60, 20);
             chatOption.TabIndex = 2;
+            chatOption.SelectedIndexChanged += new System.EventHandler(this.chatOption_SelectedIndexChanged);
 
             chatText.Location = new System.Drawing.Point(80, 522);
             chatText.Name = "채팅내용";
+            chatText.Text = "채팅내용";
             chatText.Size = new System.Drawing.Size(430, 21);
             chatText.TabIndex = 3;
             chatText.KeyUp += new System.Windows.Forms.KeyEventHandler(this.ChatKeyDown);
+            chatText.Enter += new System.EventHandler(this.chatTextEnter);
 
             Controls.Add(chattingBox);
             Controls.Add(chatOption);
@@ -100,11 +103,47 @@ namespace Avalron.Avalron
             if (chatText.Text == "")
                 return;
 
-            Avalron.gameClient.ChatSend("가나다", chatText.Text);
+            switch(Program.cmd.Splite(chatText.Text)) // 전체 채팅
+            {
+                case Command.Option.All:
+                Avalron.gameClient.ChatSend(Program.userInfo.getNick(), chatText.Text);
+                    break;
+
+                case Command.Option.Wisper:
+                    Avalron.gameClient.WisperSend(Program.userInfo.getNick(), Program.cmd.GetNick(chatText.Text), Program.cmd.GetText(chatText.Text));
+                    break;
+
+                case Command.Option.Err:
+                    return;
+
+                default:
+                    throw new Exception("먼값을 선택한거여");
+            }
 
             //chatText.Text += "\n";
             //chattingBox.Text += chatText.Text;
             chatText.Text = "";
+        }
+
+        private void chatOption_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if((string)chatOption.SelectedItem == "전체")
+            {
+                chatText.Text = "";
+            }
+            else if((string)chatOption.SelectedItem == "귓속말")
+            {
+                chatText.Text = "/w";
+            }
+        }
+
+        private void chatTextEnter(object sender, EventArgs e)
+        {
+            if (false == chatFirst)
+                return;
+
+            chatText.Text = "";
+            chatFirst = false;
         }
     }
 }
---
+++ Avalron/Command.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Avalron
+{
+    class Command
+    {
+        public enum Option { All, Wisper, Err,  NULL};
+
+        public Option Splite(string line)
+        {
+            if (0 == line.Length)
+                return Option.NULL;
+            if (line[0] != '/')
+                return Option.All;
+
+            switch (GetFirst(line))
+            {
+                case "/w":
+                case "/W":
+                case "/ㅈ":
+                    if (GetCnt(line) < 2)
+                        return Option.Err;
+                    return Option.Wisper;
+                    
+                default:
+                    return Option.Err;
+            }
+        }
+
+        private int GetCnt(string line)
+        {
+            int cnt = 0;
+            foreach(char c in line)
+            {
+                if (c == ' ')
+                    cnt++;
+            }
+            return cnt;
+        }
+
+        private string GetFirst(string line)
+        {
+            int idx = line.IndexOf(" ");
+
+            return line.Substring(0, idx);
+        }
+        
+        private string GetRest(string line)
+        {
+            int idx = line.IndexOf(" ");
+
+            return line.Substring(idx + 1, line.Length - idx);
+        }
+        
+        public string GetNick(string line)
+        {
+            string[] arr = line.Split(' ');
+
+            return arr[1];
+        }
+
+        public string GetText(string line)
+        {
+            int idx = 0;
+            for(int i =0; i < 2; i ++)
+            {
+                idx = line.IndexOf(" ");
+                line = line.Substring(idx + 1, line.Length - (idx + 1));
+            }
+
+            return line;
+        }
+    }
+}
