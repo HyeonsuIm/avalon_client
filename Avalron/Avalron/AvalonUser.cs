ec94c6b34dcd835a0713482477435dbc7bafbc8d
KimHongHoun <yumezi7@gmail.com>
2015-08-13 04:27:28 +0900

    AvalonUser : UserInfo 에서 개인에 대한 정보 단위 생성

--- Avalron/Avalron/AvalonUser.cs
+++ Avalron/Avalron/AvalonUser.cs
@@ -6,7 +6,18 @@ using System.Threading.Tasks;
 
 namespace Avalron.Avalron
 {
-    class AvalonUser
+    class AvalonUser : UserInfo
     {
+        bool Host = false;              // 호스트, 방장(서버)인지 여부
+        bool Leader = false;            // 원정대장인지 여부
+        bool Vote = false;              // 그 라운드의 투표 결과를 저장.
+        Avalron.Team team = Avalron.Team.None;  // 자신이 속한 팀이 어딘지 나타냅니다.
+        Avalron.PersonCard personCard;
+        int idNumber;                   // id의 일련번호
+
+        public AvalonUser(string id, string nick) : base(id, nick)
+        {
+
+        }
     }
 }
