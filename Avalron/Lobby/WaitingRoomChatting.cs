6f797319e2e67240cfe49b433f86088028d04112
KimHongHoun <yumezi7@gmail.com>
2015-08-22 03:14:54 +0900

    대기방 채팅을 상속으로 구현.

--- Avalron/Lobby/WaitingRoomChatting.cs
+++ Avalron/Lobby/WaitingRoomChatting.cs
@@ -7,161 +7,72 @@ using System.Windows.Forms;
 
 namespace Avalron
 {
-    class WaitingRoomChatting
+    class WaitingRoomChatting : Avalron.Chatting
     {
-        System.Windows.Forms.RichTextBox chattingBox = new RichTextBox();
-        ComboBox chatOption = new ComboBox();
-        string[] chatOptionStr = {"전체", "귓속말" };
-        TextBox chatText = new TextBox();
-        bool chatFirst = true;
-
-        public bool Closing
+        public WaitingRoomChatting(Control.ControlCollection Controls) : base(Controls)
         {
-            get; set;
-        }
-
-        public WaitingRoomChatting(Control.ControlCollection Controls)
-        {
-            chattingBox.Location = new System.Drawing.Point(15, 250);
-            chattingBox.Name = "채팅";
-            chattingBox.ReadOnly = true;
-            chattingBox.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Vertical;
-            chattingBox.Size = new System.Drawing.Size(500, 250);
-            chattingBox.TabIndex = 2;
-            chattingBox.Text = "";
-            
-            chatOption.FormattingEnabled = true;
-            chatOption.Location = new System.Drawing.Point(14, 522);
-            chatOption.Items.AddRange(chatOptionStr);
-            chatOption.SelectedIndex = 0;
-            chatOption.Name = "옵션";
-            chatOption.Size = new System.Drawing.Size(60, 20);
-            chatOption.TabIndex = 2;
-            chatOption.SelectedIndexChanged += new System.EventHandler(this.chatOption_SelectedIndexChanged);
-
-            chatText.Location = new System.Drawing.Point(80, 522);
-            chatText.Name = "채팅내용";
-            chatText.Text = "채팅내용";
-            chatText.Size = new System.Drawing.Size(430, 21);
-            chatText.TabIndex = 3;
-            chatText.KeyUp += new System.Windows.Forms.KeyEventHandler(this.ChatKeyDown);
-            chatText.Enter += new System.EventHandler(this.chatTextEnter);
-
-            Controls.Add(chattingBox);
-            Controls.Add(chatOption);
-            Controls.Add(chatText);
-            chattingBox.ResumeLayout(false);
-            chattingBox.PerformLayout();
-
-            Closing = false;
-        }
-
-        public delegate void Delegate(string text);
-
-        public void addText(string text)
-        {
-            try
-            {
-                if (this.chattingBox.InvokeRequired)
-                {
-                    chattingBox.Invoke(new Delegate(addText), new object[] { text });
-                }
-                else
-                {
-                    this.chattingBox.Text += text;
-                }
-            }
-            catch (System.Exception ex)
-            {
-                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK);
-            }
+            formNum = (int)TCPClient.FormNum.ROOM;
         }
 
         public void RunGetChat()
         {
             string getString = "";
-            while(Closing == false)
+            while(IsClosing() == false)
             {
-                try {
+                try
+                {
                     getString = Program.tcp.ReciveData() + "\n";
                 }
                 catch(System.Net.Sockets.SocketException e)
                 {
-                    //MessageBoxEx.Show(this, e.Message);
+                    MessageBoxEx.Show(e.Message);
                 }
-                 
+
                 if (getString == "")
                     continue;
+
+                Spriter spriter = new Spriter(getString);
+                int OpCode = spriter.getOpCode();
+
+                switch(OpCode)
+                {
+                    case (int)TCPClient.RoomOpCode.Chat:
+                        break;
+                    case (int)TCPClient.RoomOpCode.Wisper:
+                        break;
+                    case (int)TCPClient.RoomOpCode.Connect:
+                        break;
+                    case (int)TCPClient.RoomOpCode.DisConnect:
+                        break;
+                    case (int)TCPClient.RoomOpCode.SeatClose:
+                        break;
+                    case (int)TCPClient.RoomOpCode.Modify:
+                        break;
+                    case (int)TCPClient.RoomOpCode.Delete:
+                        break;
+                    case (int)TCPClient.RoomOpCode.Start:
+                        break;
+                    default:
+                        break;
+
+                }
                 addText(getString);
 
                 getString = "";
 
-                // 채팅 금지시
-                if(false)
+                //채팅 금지시
+                if (false)
                 {
-                    chattingBox.Enabled = false;
+                    ChatBoxEnabled = false;
                 }
 
                 // 채팅 금지 해지시
                 if(false)
                 {
-                    chattingBox.Enabled = true;
+                    ChatBoxEnabled = true;
                 }
             }
         }
-
-        private void ChatKeyDown(object sender, KeyEventArgs e)
-
-        {
-            // 엔터키만 처리합니다.
-            if (e.KeyCode !=  Keys.Enter)
-                return;
-
-            // 아무 내용 없을시 넘깁니다.
-            if (chatText.Text == "")
-                return;
-
-            switch(Program.cmd.Splite(chatText.Text)) // 전체 채팅
-            {
-                case Command.Option.All:
-                    //Program.tcp.(Program.userInfo.nick, chatText.Text);
-                    break;
-
-                case Command.Option.Wisper:
-                    //Avalron.gameClient.WisperSend(Program.userInfo.nick, Program.cmd.GetNick(chatText.Text), Program.cmd.GetText(chatText.Text));
-                    break;
-
-                case Command.Option.Err:
-                    return;
-
-                default:
-                    throw new Exception("먼값을 선택한거여");
-            }
-
-            //chatText.Text += "\n";
-            //chattingBox.Text += chatText.Text;
-            chatText.Text = "";
-        }
-
-        private void chatOption_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            if((string)chatOption.SelectedItem == "전체")
-            {
-                chatText.Text = "";
-            }
-            else if((string)chatOption.SelectedItem == "귓속말")
-            {
-                chatText.Text = "/w";
-            }
-        }
-
-        private void chatTextEnter(object sender, EventArgs e)
-        {
-            if (false == chatFirst)
-                return;
-
-            chatText.Text = "";
-            chatFirst = false;
-        }
     }
 }
+
