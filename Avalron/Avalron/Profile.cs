a245136f13468ce275280b79d44e2be9f5277a78
KimHongHoun <yumezi7@gmail.com>
2015-10-06 01:48:03 +0900

    호수의 여인은 자신 또는 이미 소지한 사람에게 사용할 수 없습니다.

--- Avalron/Avalron/Profile.cs
+++ Avalron/Avalron/Profile.cs
@@ -231,6 +231,8 @@ namespace Avalron
                         Program.avalron.gameClient.DataSend((int)Avalron.AvalronClient.EtcSpecialOpCode.LadyOfTheLakeResult, arrayIndex.ToString());
                         MessageBox.Show(arrayIndex + "호수의 여인을 이사람에게 보냄");
 
+                        Program.avalron.ladyOfTheLakeIndex = arrayIndex;
+
                         Check.Image = Properties.Resources.Avalon_투표토큰; // 임시 입니다. 호수의 여인 토큰이 와야 합니다.
                     }
                     break;
